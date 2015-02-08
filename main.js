1e96acda3c83499ee74b70d0ffd217588416cfc1
Emmanuel Mong <mongemmanuel@gmail.com>
2015-02-07 20:45:12 -0500

    After adding server start

--- main.js
+++ main.js
@@ -56,5 +56,13 @@ app.get('/temperature', function(req, res) {
     startSensorWatch(res);
 });
 
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
 
 
